f7fa858622415b83116d76435c1f27027842ea70
mathcard <mathcardoso.94@gmail.com>
2021-03-17 22:09:40 -0300

    Resolvendo exercicios

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User Not Identified'})
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if(user.pro || user.todos.length < 10) {
+    next();
+  } 
+
+  return response.status(403).json({ error: 'Your account not able more register tasks'})
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User Not Identified'})
+  }
+
+  const validateId = validate(id, 4);
+
+  if(!validateId) {
+    return response.status(400).json({ error: 'Invalid ID'});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Todo Not Exist'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(user) {
+    request.user = user;
+    next();    
+  }
+
+  return response.status(404).json({ error: 'User Not Identified'});
+  
 }
 
 app.post('/users', (request, response) => {
